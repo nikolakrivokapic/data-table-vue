9b4e73e9de117fe9e4baef38959565a8e7a89b61
Nikola Krivokapic <nikolakrivokapic84@gmail.com>
2018-12-27 00:30:16 +0100

    Quiz update

--- quiz.js
+++ quiz.js
@@ -1,9 +1,8 @@
-// Assuming you have already done "npm install fernet"
 const fernet = require('fernet');
 const secret = new fernet.Secret('TluxwB3fV_GWuLkR1_BzGs1Zk90TYAuhNMZP_0q4WyM=');
-// Oh no! The code is going over the edge! What are you going to do?
-const message = `gAAAAABcFXX6stnWylkawby4yUTfjd6ztVQvm--bMJR8fMosu_4F2fhcqujwvdwzR7zFtPXEOCaVIr9i8MnFJkUzM8XR53
-               LwaHMexn1MZzJlSZlp8gU8niSPaCcgrkj9xNMzOqZsJkT2KfVq4SxWSMloER6O6YqXjzdJyhFfxXvN6AOzltwIJ_mpGVNY
-               CFQJaPzr-d08etUs`;
-const url = new fernet.Token({secret, token: message, ttl:0});
+const token = `gAAAAABcJA208va-jfo6GFMVgGOLDsg9TCEtwkvCtsLl72pIWgcDK7fO1Nx6uSWp_Em_k-c1IMLBTw-iqUrhpYHzL2K8K0AjofEn
+            -j4kCFT3POx5ukQJbzZyQ-WLrJklcZTPp09xA3_kRswPBUozi0Qft3PC3YhzaO-_KKEMYXYvH1xrJHjssb-28_gEezfKW-cNb5MUG9do`;
+
+const url = new fernet.Token({secret, token, ttl:0});
 console.log(url.decode());
+
